a5ab615ac4d4e50443a7e5735b69a21472a19d81
Tim Bell <tim.bell02@gmail.com>
2016-06-02 22:46:29 +0000

    added new entry

--- src/client/app/portfoliodata.js
+++ src/client/app/portfoliodata.js
@@ -1,5 +1,13 @@
 let portfoliodata = [
     {
+        name: 'Roguelike Dungeon Crawler',
+        img: './src/client/public/css/port-images/roguelike.png',
+        skills: 'HTML/CSS,JavaScript,React,Redux,SASS',
+        time: 'late',
+        github: 'https://github.com/MindfulBell/roguelike2',
+        active: 'https://htmlpreview.github.io/?https://github.com/MindfulBell/roguelike2/blob/master/src/client/index.html'
+    },
+    {
         name: 'Recipe Box',
         img: './src/client/public/css/port-images/recipe-box.png',
         skills: 'HTML/CSS,JavaScript,Bootstrap,React,SASS,local storage',
